4a34a41cebc9b423841d2412323d40d3428f49c1
EmreBasar98 <emre.basar.98@gmail.com>
2024-01-16 23:50:12 +0300

    27.12.23

---
+++ src/solved/BestDigits.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BestDigits {
+
+
+    public static void main(String[] args) {
+        BestDigits bestDigits = new BestDigits();
+        System.out.println(bestDigits.bestDigits("462839", 2));
+    }
+
+
+    public String bestDigits(String number, int numDigits) {
+        List<Character> digitStack = new ArrayList<>();
+        for (Character digit: number.toCharArray()) {
+            if (digitStack.isEmpty()) digitStack.add(digit);
+            else {
+                while (numDigits > 0 && digitStack.size() > 0 && digitStack.get(digitStack.size() - 1) <= digit) {
+                    digitStack.remove(digitStack.size() - 1);
+                    numDigits--;
+                }
+                digitStack.add(digit);
+            }
+        }
+        while (numDigits > 0 && digitStack.size() > 0) {
+            digitStack.remove(digitStack.size() - 1);
+            numDigits--;
+        }
+        return digitStack.stream().map(String::valueOf).reduce("", String::concat);
+    }
+}
